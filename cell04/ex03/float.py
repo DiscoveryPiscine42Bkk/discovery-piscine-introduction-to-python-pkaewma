79e125d0b9218ab4c48b11f69d8a86c076aff46c
pkaewma <pattanankaewma@gmail.com>
2025-06-17 14:29:38 +0700

    Create float.py

---
+++ cell04/ex03/float.py
@@ -0,0 +1,15 @@
+from cmath import inf
+
+
+user_input = input("Please enter a number")
+try:
+    number = float(user_input)
+    if number . is_integer():
+        print(f"The number {inf(number)} is an integer.")
+    elif '.' in user_input:
+        print(f"The number {user_input} is a decimal")
+    else:
+        print(f"The number {user_input} is not a decimal")
+
+except ValueError:
+    print("That's not a valid number.")

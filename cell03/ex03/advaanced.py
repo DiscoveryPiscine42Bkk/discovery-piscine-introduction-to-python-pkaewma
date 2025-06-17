64521f56dd815a58e283a755f6c2ffec6a84eb45
pkaewma <pattanankaewma@gmail.com>
2025-06-17 13:32:08 +0700

    Create advaanced.py

---
+++ cell03/ex03/advaanced.py
@@ -0,0 +1,15 @@
+from unittest import result
+
+def print_multiplicaion_tables():
+    print("multiplicaion Tables")
+    print("--------------------")
+
+    for i in range (1,11):
+        print(f"Table for {i}:")
+        for j in range(1,11):
+            resuit = i * j 
+            print(f" {i} x {j} = {result}")
+
+    print("")
+
+print_multiplicaion_tables()
